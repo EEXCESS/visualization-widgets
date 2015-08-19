f9a0c8c209f03950adde1a9416e22294d2e20268
gerwald <gerwald.tschinkel@interad.at>
2015-08-19 15:06:57 +0200

    code cleanup

--- Dashboard/js/pluginhandler.js
+++ Dashboard/js/pluginhandler.js
@@ -1,88 +1,88 @@
 var PluginHandler = {
-	vis : null,
-	visPlugins:[],
-	filterPlugins:[],
-	visRootSelector: null,
-	filterRootSelector: null,
-	pluginScripts:[],
-	isInitialized:false,
-	defaultCombinations : [
-                [
-                    {"facet": "language", "visualattribute": "x-axis"},
-                    {"facet": "count", "visualattribute": "y-axis"},
-                    {"facet": "language", "visualattribute": "color"}
-                ],
-                [
-                    {"facet": "provider", "visualattribute": "x-axis"},
-                    {"facet": "count", "visualattribute": "y-axis"},
-                    {"facet": "provider", "visualattribute": "color"}
-                ]
-            ],
+    vis: null,
+    visPlugins: [],
+    filterPlugins: [],
+    visRootSelector: null,
+    filterRootSelector: null,
+    pluginScripts: [],
+    isInitialized: false,
+    defaultCombinations: [
+        [
+            { "facet": "language", "visualattribute": "x-axis" },
+            { "facet": "count", "visualattribute": "y-axis" },
+            { "facet": "language", "visualattribute": "color" }
+        ],
+        [
+            { "facet": "provider", "visualattribute": "x-axis" },
+            { "facet": "count", "visualattribute": "y-axis" },
+            { "facet": "provider", "visualattribute": "color" }
+        ]
+    ],
 
-	initialize:function(vis, visRootSelector, filterRootSelector){
-		PluginHandler.vis = vis;
-		PluginHandler.visRootSelector = visRootSelector;
-		PluginHandler.filterRootSelector = filterRootSelector;
-		PluginHandler.isInitialized = true;
-		
-		PluginHandler.registerPluginScripts(PluginHandler.pluginScripts);		
-	},
+    initialize: function (vis, visRootSelector, filterRootSelector) {
+        PluginHandler.vis = vis;
+        PluginHandler.visRootSelector = visRootSelector;
+        PluginHandler.filterRootSelector = filterRootSelector;
+        PluginHandler.isInitialized = true;
 
-	getPlugins:function(){
-		return PluginHandler.visPlugins;
-	},
+        PluginHandler.registerPluginScripts(PluginHandler.pluginScripts);
+    },
 
-	registerVisualisation: function(pluginObject, configuration){
+    getPlugins: function () {
+        return PluginHandler.visPlugins;
+    },
 
-		if (!configuration.mappingCombinations){
-			configuration.mappingCombinations = PluginHandler.defaultCombinations;
-		}
+    registerVisualisation: function (pluginObject, configuration) {
 
-		configuration.Object = pluginObject;
-		if (configuration.Object.initialize != undefined)
-			configuration.Object.initialize(PluginHandler.vis, PluginHandler.visRootSelector);
-		PluginHandler.visPlugins.push(configuration);
-		PluginHandler.vis.refreshChartSelect(); // todo: call not before all plugins are loaded
-	},
+        if (!configuration.mappingCombinations) {
+            configuration.mappingCombinations = PluginHandler.defaultCombinations;
+        }
 
-	registerFilterVisualisation: function(pluginObject, configuration){
-		configuration.Object = pluginObject;
-		PluginHandler.filterPlugins.push(configuration);
-	},
+        configuration.Object = pluginObject;
+        if (configuration.Object.initialize != undefined)
+            configuration.Object.initialize(PluginHandler.vis, PluginHandler.visRootSelector);
+        PluginHandler.visPlugins.push(configuration);
+        PluginHandler.vis.refreshChartSelect(); // todo: call not before all plugins are loaded
+    },
 
-	getFilterPluginForType: function(type){
-		for(var i=0; i<PluginHandler.filterPlugins.length; i++){
-			var filterPlugin = PluginHandler.filterPlugins[i];
-			if (filterPlugin.type == type)
-				return filterPlugin;
-		}
-	},
+    registerFilterVisualisation: function (pluginObject, configuration) {
+        configuration.Object = pluginObject;
+        PluginHandler.filterPlugins.push(configuration);
+    },
 
-	registerPluginScripts:function(pluginScripts){		
-		for(var i=0; i<pluginScripts.length; i++){
-			if (PluginHandler.isInitialized){
-				PluginHandler.loadScript(pluginScripts[i]);
-			} else {
-				PluginHandler.pluginScripts.push(pluginScripts[i]);
-			}
-		}			
-	},
+    getFilterPluginForType: function (type) {
+        for (var i = 0; i < PluginHandler.filterPlugins.length; i++) {
+            var filterPlugin = PluginHandler.filterPlugins[i];
+            if (filterPlugin.type == type)
+                return filterPlugin;
+        }
+    },
 
-	loadScript: function (scriptName, callback) {
-	    var scriptEl = document.createElement('script');
-	    //scriptEl.src = chrome.extension.getURL('visualizations/Vis-Template/Plugins/' + scriptName + '');
-		scriptEl.src = 'Plugins/' + scriptName + '';
-	    scriptEl.addEventListener('load', callback, false);
-	    document.head.appendChild(scriptEl);
-	},
+    registerPluginScripts: function (pluginScripts) {
+        for (var i = 0; i < pluginScripts.length; i++) {
+            if (PluginHandler.isInitialized) {
+                PluginHandler.loadScript(pluginScripts[i]);
+            } else {
+                PluginHandler.pluginScripts.push(pluginScripts[i]);
+            }
+        }
+    },
 
-	getByDisplayName:function(displayName){
-		for(var i=0; i<PluginHandler.visPlugins.length; i++){
-			var plugin = PluginHandler.visPlugins[i];
-			if (plugin.displayName == displayName){				
-				return plugin;			
-			}
-		}
-		return null;
-	}
+    loadScript: function (scriptName, callback) {
+        var scriptEl = document.createElement('script');
+        //scriptEl.src = chrome.extension.getURL('visualizations/Vis-Template/Plugins/' + scriptName + '');
+        scriptEl.src = 'Plugins/' + scriptName + '';
+        scriptEl.addEventListener('load', callback, false);
+        document.head.appendChild(scriptEl);
+    },
+
+    getByDisplayName: function (displayName) {
+        for (var i = 0; i < PluginHandler.visPlugins.length; i++) {
+            var plugin = PluginHandler.visPlugins[i];
+            if (plugin.displayName == displayName) {
+                return plugin;
+            }
+        }
+        return null;
+    }
 };
