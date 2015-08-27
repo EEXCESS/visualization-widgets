33a1d6732376f81f9cbc32a4baeb4503e005c831
Gerwald Tschinkel <gtschinkel@know-center.at>
2015-08-27 12:21:07 +0200

    Cleanup unused Mockup Files

--- Dashboard/plugins.js
+++ Dashboard/plugins.js
@@ -1,10 +1,8 @@
 PluginHandler.registerPluginScripts([
 		'TestPlugin.js',
-		//'MiniVizTimeMockup.js' ,
 		//'TestMiniVizGeo.js' ,
 		'TestMiniVizList.js' ,
 		'FilterVisCategoryHex.js',
 		'FilterVisTime.js',
 		'FilterVizGeo.js'
-		//'MiniVizCategoryMockup.js'
 	]);
