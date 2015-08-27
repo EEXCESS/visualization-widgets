2ed46e3d1482ba9a9c406aebf2ef905f2d1ba96f
Gerwald Tschinkel <gtschinkel@know-center.at>
2015-08-27 17:12:18 +0200

    Usage Collector, work started...

---
+++ Dashboard/js/usagecollector.js
@@ -0,0 +1,10 @@
+var UsageCollector =  {
+
+    // Actions: chartChanged, document-link-clicked, bookmark-clicked; boomkark-created, bookmark-collection-created; 
+    // config-changed, filter-created, filter-changed. filter-saved.
+    // collect('chart-clicked', 'barchart')
+    // collect(action, additionalInformation) + timestamp
+    // --> push to stack. if stack has 10 elements, or no collection of about 10
+    // --> send it to a centralized server --> what server???
+    
+};
