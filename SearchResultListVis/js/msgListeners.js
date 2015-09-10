85ac43876ef9cd9194b9cab1c96a37bc39fb6a87
Lisa Wagner <github@lisawagner.li>
2015-09-10 12:44:17 +0200

    fixing overlapping issue by adding imagesloaded check as recommended by isotope

--- SearchResultListVis/js/msgListeners.js
+++ SearchResultListVis/js/msgListeners.js
@@ -2,9 +2,21 @@ window.onmessage = function (msg) {
     if (msg.data.event) {
         if (msg.data.event === 'eexcess.newSearchTriggered') {
             // new search has been triggered somewhere, show loading bar or similar
+
         }
         else if (msg.data.event === 'eexcess.newResults') {
             // new results are available in msg.data.data
+
+            //show favicons
+//                $.each(msg.data.data.result, function (idx, val) {
+//                    $('body').append('<img src="' + msg.data.data.faviconURL + val.documentBadge.provider + '" />');
+//                });
+            //remove old data
+            $("img").remove(".grid-item");
+
+            var $items = $(addImages());
+
+            //init isotope
             $('.grid').isotope({
                 itemSelector: '.grid-item',
                 percentPosition: true,
@@ -13,15 +25,10 @@ window.onmessage = function (msg) {
                 }
             });
 
-            //show favicons
-//                $.each(msg.data.data.result, function (idx, val) {
-//                    $('body').append('<img src="' + msg.data.data.faviconURL + val.documentBadge.provider + '" />');
-//                });
-
-
-            //show images
-            console.log(msg.data.data);
-            addImages();
+            //check if all items are loaded to avoid overlap, then add items to container
+            $items.imagesLoaded(function () {
+                $('.grid').isotope('insert', $items);
+            });
 
             // get details for all results
             var documentBadges = [];
@@ -52,11 +59,6 @@ window.onmessage = function (msg) {
             }
 
         });
-
-        var $items = $(items);
-        // append elements to container
-        $('.grid').append($items)
-            // add and lay out newly appended elements
-            .isotope('appended', $items);
+        return items;
     }
 };
