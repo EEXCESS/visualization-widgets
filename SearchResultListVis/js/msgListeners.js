2300919b3f5c33971567267430493e82ca470698
Lisa Wagner <github@lisawagner.li>
2015-09-16 21:21:29 +0200

    added media type colors to filters

--- SearchResultListVis/js/msgListeners.js
+++ SearchResultListVis/js/msgListeners.js
@@ -2,7 +2,17 @@ window.onmessage = function (msg) {
     if (msg.data.event) {
         if (msg.data.event === 'eexcess.newSearchTriggered') {
             // new search has been triggered somewhere, show loading bar or similar
-
+            //    helper.showLoadingScreen();
+            //    $('#result_gallery').remove();
+            //    $('#eexcess_thumb').hide();
+            //    settings.hostTag.find('.pagination').remove();
+            //    $widgets.error.hide();
+            //    $widgets.list.empty();
+            //    $('.empty_result').hide();
+            //    $widgets.loader.show();
+            //};
+            //$('.eexcess-isotope-grid').append('<div class="eexcess_loading" style="display:none"><img src="' + settings.pathToMedia + 'loading.gif"' +
+            //    ' /></div>');
         }
         else if (msg.data.event === 'eexcess.newResults') {
             // new results are available in msg.data.data
