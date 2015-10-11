6f9486ce5c20e891c3d12910372f76e67307736e
Lisa Wagner <github@lisawagner.li>
2015-10-11 13:08:22 +0200

    result items are now links (which open in a new tab)

--- SearchResultListVis/js/helperMethods.js
+++ SearchResultListVis/js/helperMethods.js
@@ -69,14 +69,22 @@ function addGridResultItems(msg) {
 
     $.each(msg.data.data.result, function (idx, val) {
 
+
+
+            //assemble href for item
+            var itemLink = ' <a target="_blank" href="' + val.documentBadge.uri + '"><span class="emptyspan"></span>';
+
+            // get title to adjust its length according to the space available in the media type's description
+            var itemTitle = val.title;
+
+
             // add isotoped items
             if (val.mediaType == "IMAGE" || val.mediaType == "image") {
                 var previewImage;
 
-                // adjust title length to fit grid item description, this is done for every major media type
-                var imageItemTitle = val.title;
-                if (imageItemTitle.length > 50) {
-                    imageItemTitle = imageItemTitle.substring(0, 49) + "...";
+
+                if (itemTitle.length > 50) {
+                    itemTitle = itemTitle.substring(0, 49) + "...";
                 }
 
                 if (val.previewImage != undefined) {
@@ -84,31 +92,32 @@ function addGridResultItems(msg) {
                 } else {
                     previewImage = "http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getPreviewImage?type=image";
                 }
-                var item = '<div class = "eexcess-isotope-grid-item eexcess-image"  data-category="eexcess-image"> <div class="description-image"> <!-- description' +
-                    ' content -->  <p class="description_content title">' + imageItemTitle +
+                var item = '<div class = "eexcess-isotope-grid-item eexcess-image"  data-category="eexcess-image">' + itemLink + ' <div class="description-image"> <!-- description' +
+                    ' content -->  <p class="description_content title">' + itemTitle +
                     '</p><!-- end description content -->   </div>' +
                     '  <img src="' + previewImage + '" /> </div>';
+
 
                 items += item;
             }
 
             else if (val.mediaType == "TEXT" || val.mediaType == "text") {
 
-                var textItemTitle = val.title;
-                if (textItemTitle.length > 100) {
-                    textItemTitle = textItemTitle.substring(0, 99) + "...";
+                if (itemTitle.length > 100) {
+                    itemTitle = itemTitle.substring(0, 99) + "...";
                 }
 
                 if (val.date != "unknown") {
-                    var item = '<div class = "eexcess-isotope-grid-item eexcess-text" data-category="eexcess-text">' +
+                    var item = '<div class = "eexcess-isotope-grid-item eexcess-text" data-category="eexcess-text">' + itemLink +
+
                         ' <div class="description-text">' +
-                        ' <p class="description_content title"><b>Title:</b> ' + textItemTitle + "<br><b>Provider:</b> " + val.documentBadge.provider + " <br><b>Date:</b> " + val.date +
+                        ' <p class="description_content title"><b>Title:</b> ' + itemTitle + "<br><b>Provider:</b> " + val.documentBadge.provider + " <br><b>Date:</b> " + val.date +
                         '</p></div><img src="' + 'https://dl.dropboxusercontent.com/u/25937134/TextButton.png' + '" /></div>';
                     items += item;
                 } else {
-                    var item = '<div class = "eexcess-isotope-grid-item eexcess-text" data-category="eexcess-text">' +
+                    var item = '<div class = "eexcess-isotope-grid-item eexcess-text" data-category="eexcess-text">' + itemLink +
                         ' <div class="description-text">' +
-                        ' <p class="description_content title"><b>Title:</b> ' + textItemTitle + "<br><b>Provider:</b> " + val.documentBadge.provider +
+                        ' <p class="description_content title"><b>Title:</b> ' + itemTitle + "<br><b>Provider:</b> " + val.documentBadge.provider +
                         '</p></div><img src="' + 'https://dl.dropboxusercontent.com/u/25937134/TextButton.png' + '" /></div>';
                     items += item;
                 }
@@ -117,33 +126,33 @@ function addGridResultItems(msg) {
 
             else if (val.mediaType == "AUDIO" || val.mediaType == "audio") {
 
-                var item = '<div class = "eexcess-isotope-grid-item eexcess-audio" data-category="eexcess-audio">' +
+                var item = '<div class = "eexcess-isotope-grid-item eexcess-audio" data-category="eexcess-audio">' + itemLink +
                     ' <div' +
                     ' class="description">' +
-                    ' <p class="description_content title">' + val.title.substring(0, 49) + "..." +
+                    ' <p class="description_content title">' + itemTitle+
                     '</p></div><img src="' + 'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getPreviewImage?type=audio' + '" /></div>';
                 items += item;
             }
             else if (val.mediaType == "VIDEO" || val.mediaType == "video") {
 
-                var item = '<div class = "eexcess-isotope-grid-item eexcess-video" data-category="eexcess-video">' +
+                var item = '<div class = "eexcess-isotope-grid-item eexcess-video" data-category="eexcess-video">' + itemLink +
                     ' <div class="description">' +
-                    ' <p class="description_content title">' + val.title.substring(0, 49) + "..." +
+                    ' <p class="description_content title">' + itemTitle+
                     '</p></div><img src="' + 'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getPreviewImage?type=video' + '" /></div>';
                 items += item;
             }
 
             else if (val.mediaType == "3D" || val.mediaType == "3d") {
 
-                var item = '<div class = "eexcess-isotope-grid-item eexcess-3d" data-category="eexcess-3d"> <div class="description">' +
-                    ' <p class="description_content title">' + val.title.substring(0, 49) + "..." +
+                var item = '<div class = "eexcess-isotope-grid-item eexcess-3d" data-category="eexcess-3d"> ' + itemLink + ' <div class="description">' +
+                    ' <p class="description_content title">' + itemTitle+
                     '</p></div><img src="http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getPreviewImage?type=3d' + '" / > < / div > ';
                 items += item;
             }
 
             else {
-                var item = '<div class = "eexcess-isotope-grid-item eexcess-unknown" data-category="eexcess-unknown"-><div class="description"> <p' +
-                    ' class="description_content title">' + val.title.substring(0, 49) + "..." +
+                var item = '<div class = "eexcess-isotope-grid-item eexcess-unknown" data-category="eexcess-unknown"->' + itemLink + '<div class="description"> <p' +
+                    ' class="description_content title">' + itemTitle+
                     '</p></div> <img src="' + 'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getPreviewImage?type=unknown' + '" /></div>';
                 items += item;
             }
