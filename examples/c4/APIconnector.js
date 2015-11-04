a5e8f8c3873d2df276b6d28b467134eefb2427e8
Gerwald Tschinkel <gerwald.tschinkel@interad.at>
2015-11-04 09:01:47 +0100

    switch to STABLE endpoint

--- examples/c4/APIconnector.js
+++ examples/c4/APIconnector.js
@@ -13,10 +13,10 @@
 
 define(['jquery'], function($) {
     var settings = {
-        base_url: 'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy-1.0-SNAPSHOT/api/v1/',
+        base_url: 'http://eexcess.joanneum.at/eexcess-privacy-proxy-issuer-1.0-SNAPSHOT/api/v1/',
         favicon_url: 'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getPartnerFavIcon?partnerId=',
         timeout: 10000,
-        cacheSize: 10,
+        cacheSize: 10, 
         suffix_recommend: 'recommend',
         suffix_details: 'getDetails'
     };
