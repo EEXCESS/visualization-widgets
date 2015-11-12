e97c51e426d46245688dfcf345ed4a47203dae9e
Gerwald Tschinkel <gtschinkel@know-center.at>
2015-11-12 10:52:43 +0100

    missing semicolon

--- Dashboard/starter.js
+++ Dashboard/starter.js
@@ -337,7 +337,7 @@ function requestPlugin() {
     for (var i=0; i<detailData.documentBadge.length; i++){
         var detailDataItem = detailData.documentBadge[i];
         //var details = JSON.parse(detailDataItem.detail);
-        var originalItem = _.find(data, function(dataItem){ return dataItem.documentBadge.id == detailDataItem.id; })
+        var originalItem = _.find(data, function(dataItem){ return dataItem.documentBadge.id == detailDataItem.id; });
         //originalItem.details = details;
     }
     
