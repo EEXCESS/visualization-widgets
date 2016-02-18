eb91dbbad1204050a8bcc8a1edbbfa97c6d9be6b
Gerwald Tschinkel <gtschinkel@know-center.at>
2016-02-18 14:06:33 +0100

    updated examples c4 Library to 6.6 for using enhanced log forwarding

--- examples/bower_components/c4/CitationBuilder/CitationBuilder/compileLib.sh
+++ examples/bower_components/c4/CitationBuilder/CitationBuilder/compileLib.sh
@@ -1,2 +1,16 @@
 cd js
-cat xmldom.js citeproc.js citeprocConsts.js CLSWrapper.js > ../citationBuilder.js
+
+if [ "$1" == "--amd" ] && [ "$#" == 1 ]; then
+   echo "define([], function(){" > ../citationBuilder.js
+   cat xmldom.js citeproc.js citeprocConsts.js CLSWrapper.js >> ../citationBuilder.js
+   printf "return CitationProcessor;\n});" >> ../citationBuilder.js
+else
+   if [ $# == 0 ]; then
+      cat xmldom.js citeproc.js citeprocConsts.js CLSWrapper.js > ../citationBuilder.js
+   else
+         echo "
+Usage: compileLib [--amd] 
+--amd:     Optional parameter. Determines whether the lib is amd compatible
+         "
+   fi
+fi
