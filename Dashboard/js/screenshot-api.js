131d1c2705813a00edad99c7126a79eb1e0b41cf
Peter Hasitschka <phasitschka@know-center.at>
2016-04-15 18:55:35 +0200

    further tryouts with canvas rendering

--- Dashboard/js/screenshot-api.js
+++ Dashboard/js/screenshot-api.js
@@ -10,13 +10,29 @@ SS.Screenshot.prototype.createDemoButton = function () {
     jQuery(document).ready(function () {
         var parent_elm = jQuery('#eexcess_controls');
         parent_elm.append(jQuery("<div class='screenshot_demobutton'><a href='#'>Take Screenshot</a>"));
-    });
+
+        jQuery('body').append(jQuery("<canvas id='my-canvas'></canvas>"));
+        jQuery('.screenshot_demobutton').click(function () {
+            this.screenshot();
+        }.bind(this));
+    }.bind(this));
 
 };
 
 
 
+SS.Screenshot.prototype.screenshot = function () {
 
+
+
+    var canvas = document.getElementById('my-canvas');
+    var ctx = canvas.getContext("2d");
+// Draw the window at the top left of canvas, width=100, height=200, white background
+
+    ctx.drawWindow(window, 0, 0, 100, 200, "rgb(255,255,255)");
+// Open another window with the thumbnail as an image
+    open(canvas.toDataURL("image/png"));
+};
 
 
 
