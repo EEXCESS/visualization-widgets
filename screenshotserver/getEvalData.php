e2a1f85af58c00bdaa3f817c97b76a30d8700aed
Peter Hasitschka <phasitschka@know-center.at>
2016-04-22 10:37:35 +0200

    Added user selection for evaluation

--- screenshotserver/getEvalData.php
+++ screenshotserver/getEvalData.php
@@ -1,9 +1,19 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-$data = file_get_contents("evalsettings.json");
-echo $data;
+echo json_encode(getEvalData());
 
+function getEvalData() {
+    $data = file_get_contents("evalsettings.json");
+    $data = json_decode($data);
 
+    $userId = $data->userId;
+    $userName = $data->users->$userId;
 
+    unset($data->users);
+    $data->user = $userName;
+
+    return $data;
+}
--- screenshotserver/setEvalData.php
+++ screenshotserver/setEvalData.php
@@ -2,12 +2,29 @@
 $settingsFile = "evalsettings.json";
 
 $msgs = array();
+
+
+$data = file_get_contents($settingsFile);
+$data = json_decode($data);
+
 if ($_POST) {
-    $data = new stdClass();
-    $data->userId = substr($_POST["user"], 0, 50);
-    $data->textualFilterMode = substr($_POST["textualfiltermode"], 0, 12);
+
+    $postData = $_POST;
+
+    if ($postData["user_id"] !== "new") {
+        $data->userId = intval($postData["user_id"]);
+    } else {
+
+
+        $newId = sizeof((array)$data->users);
+        //echo "NEW ID: " . $newId;
+        $data->users->$newId = substr($postData["user_new"], 0, 12);
+        $data->userId = $newId;
+    }
+    $data->textualFilterMode = substr($postData["textualfiltermode"], 0, 12);
 
     $succesWriteFile = file_put_contents($settingsFile, json_encode($data));
+
     if ($succesWriteFile)
         $msgs[] = "Daten <strong style='color:green;'>gespeichert!</strong>" . json_encode($data);
     else
@@ -15,8 +32,6 @@ if ($_POST) {
 }
 
 
-$currData = file_get_contents($settingsFile);
-$currData = json_decode($currData);
 
 
 $filterModes = array(
@@ -30,6 +45,15 @@ $filterModes = array(
 <html>
 
     <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+        <script>
+            jQuery(document).ready(function () {
+                jQuery('#new_user_text').click(function () {
+                    jQuery('#new_user_radio').prop("checked", true);
+                });
+            });
+        </script>
     </head>
 
 
@@ -46,16 +70,27 @@ $filterModes = array(
 
         <form METHOD ="POST" style="border: 1px solid; width:80%;  float:left; background:#E8E8E8; padding:30px">
             <h2>User-Settings</h2>
-            <p>User-ID: <input type="text" name="user" value="<?php echo $currData->userId; ?>" /></p>
+            <p>User-ID: <br/>
+                <?php foreach ($data->users as $userKey => $aUser) : ?>
+                    <input name="user_id" type="radio" value="<?php echo $userKey; ?>" 
+                    <?php echo (intval($userKey) === intval($data->userId) ? "checked='checked'" : "") ?>
+                           /> 
+
+                    <?php echo $userKey . " " . $aUser; ?><br/>
+                <?php endforeach; ?>
+
+                <input name="user_id" id="new_user_radio" type="radio" value="new" />
+                <input type="text" id="new_user_text" name="user_new" value="" placeholder="Neuer User"/></p>
             <p>Textual-Filter-Mode: 
                 <select name="textualfiltermode">
                     <?php foreach ($filterModes as $filter) : ?>
                         <option value="<?php echo $filter ?>"
-                                <?php echo ($filter === $currData->textualFilterMode ? "selected='selected'" : "") ?>>
+                                <?php echo ($filter === $data->textualFilterMode ? "selected='selected'" : "") ?>>
                             <?php echo $filter ?></option>
                     <?php endforeach; ?>
                 </select>
             </p>
+
             <button type="submit">Aktualisieren</button>
         </form>
     </body>
