f20ab0d57013107d469842c3c464d9544e1ab586
Peter Hasitschka <phasitschka@know-center.at>
2016-05-03 08:24:22 +0200

    Saving bookmarks

--- collaborativebookmarking/bookmarks.php
+++ collaborativebookmarking/bookmarks.php
@@ -2,6 +2,8 @@
 
 header('Access-Control-Allow-Origin: *');
 
+$folder = "bookmarks";
+
 $post = $_POST;
 
 $out = array();
@@ -20,17 +22,42 @@ if (!isset($post["method"]))
 
 if ($post["method"] === "storebms")
 {
-    var_dump($post);
-    
-    
-    
+    if (!isset($post["user"]))
+    {
+        $out["msg"] = "ERROR: NO USER FOUND!";
+        $out["error"] = true;
+        echo json_encode($out);
+        exit;
+    }
+
+    if (!isset($post["bms"]))
+    {
+        $out["msg"] = "ERROR: NO BOOKMARKS FOUND!";
+        $out["error"] = true;
+        echo json_encode($out);
+        exit;
+    }
+
+
+    $bm_content = json_encode($post["bms"]);
+
+    $userId = $post["user"];
+
+    $filename = $folder . "/" . md5($userId) . ".json";
+
+    $success = file_put_contents($filename, $bm_content);
+
+    if (!$success)
+    {
+        $out["msg"] = "ERROR: COULD NOT SAVE BOOKMARKS TO FILE ".$filename."!" ;
+        $out["error"] = true;
+        echo json_encode($out);
+        exit;
+    }
 }
 
 
 if ($post["method"] === "getbms")
 {
-    
-    
-    
     
 }
